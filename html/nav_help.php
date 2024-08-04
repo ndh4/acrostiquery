f0fd7f7ec9f66b0588f1b96aacf80b2d3a4a4ecd
Nathaniel Hejduk <63759434+ndh4@users.noreply.github.com>
2024-08-04 12:41:09 -0500

    Delete html directory

--- html/nav_help.php
+++
@@ -1,8 +0,0 @@
-
-			<div id="nav_sub2">
-				<ul>
-					<li><a href="<?php echo $url_html.'/help.php' ?>">Basic Search</a></li>
-					<li><a href="<?php echo $url_html.'/help_advanced.php' ?>">Advanced Search</a></li>
-					<li><a href="<?php echo $url_html.'/help_results.php' ?>">Results Page</a></li>
-				</ul>
-			</div>
--- html/nav_lang_sources.php
+++
@@ -1,5 +0,0 @@
-			<div id="nav_sub2">
-				<ul>
-					<li><a href="<?php echo $url_html.'/dates.php'; ?>">Dates</a></li>
-				</ul>
-			</div>
