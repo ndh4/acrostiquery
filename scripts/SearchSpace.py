374904cc72065ddd4349542f4bebef2c2849c434
ndh4 <ndh4@rice.edu>
2024-09-10 16:44:06 -0500

    Start implementing DirectorySearchSpace

--- scripts/SearchSpace.py
+++ scripts/SearchSpace.py
@@ -1,3 +1,5 @@
+import os
+import re
 from abc import abstractmethod
 from data_types import Line
 
@@ -7,9 +9,22 @@ class SearchSpace:
     def reset(self):
         pass
 
+
     @abstractmethod
     def get_next_line(self) -> Line | None:
         pass
+
+
+    @abstractmethod
+    def clean_up(self):
+        pass
+
+
+def make_search_space(pathname: str) -> SearchSpace:
+    if os.path.isdir(pathname):
+        return DirectorySearchSpace(pathname)
+    if os.path.isfile(pathname):
+        return FileSearchSpace(pathname)
 
 
 class FileSearchSpace(SearchSpace):
@@ -18,10 +33,11 @@ class FileSearchSpace(SearchSpace):
         self.file = None
         return
 
+
     def reset(self):
-        if self.file is not None:
-            self.file.close()
+        self.clean_up()
         self.file = open(self.pathname, 'r')
+
 
     def get_next_line(self) -> Line | None:
         while True:
@@ -37,13 +53,38 @@ class FileSearchSpace(SearchSpace):
             self.file.close()
             self.file = None
 
+def name_to_int(name: str) -> int:
+    result = re.search('\d+', name)
+    if result is None:
+        return -1
+    else:
+        return int(result.group())
 
 class DirectorySearchSpace(SearchSpace):
-    def __init__(self, pathname: str):
+    def __init__(self, dirpath: str):
+        self.spaces = [make_search_space(os.path.join(dirpath, subpath))
+                       for subpath in
+                       sorted(os.listdir(dirpath), key=name_to_int)]
+        self.current_space = -1
         return
 
+
     def reset(self):
-        pass
+        self.clean_up()
+
 
     def get_next_line(self) -> Line | None:
-        pass
+        while -1 < self.current_space < len(self.spaces):
+            line_candidate = self.spaces[self.current_space].get_next_line()
+            if line_candidate:
+                return line_candidate
+            else:
+                self.spaces[self.current_space].clean_up()
+                self.current_space += 1
+                self.spaces[self.current_space].reset()
+        return None
+
+
+    def clean_up(self):
+        self.spaces[self.current_space].clean_up()
+        self.current_space = -1
--- tests/do_search_tests.py
+++ tests/do_search_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from SearchSpace import SearchSpace, FileSearchSpace
+from SearchSpace import SearchSpace, FileSearchSpace, make_search_space
 from do_search import do_search, get_hit_indices
 
 
@@ -8,13 +8,26 @@ class SearchTests(unittest.TestCase):
 
     def test_do_search(self):
         path = '../texts/la/lucan.bellum_civile/lucan.bellum_civile.part.1.tess'
-        search_space = FileSearchSpace(path)
+        search_space = make_search_space(path)
         rich_hits = do_search('ACROSTIC', 'la', search_space, 'HUQ', 2)
         for hit in rich_hits:
             hit.print()
 
     def test_get_hit_indices(self):
         path = '../texts/la/lucan.bellum_civile/lucan.bellum_civile.part.1.tess'
-        search_space = FileSearchSpace(path)
+        search_space = make_search_space(path)
         hits = get_hit_indices('ACROSTIC', 'la', search_space, 'HUQ')
         self.assertEqual(hits, {13})
+
+    def test_do_search_dir(self):
+        path = '../texts/la/lucan.bellum_civile'
+        search_space = make_search_space(path)
+        rich_hits = do_search('ACROSTIC', 'la', search_space, 'HUQ', 2)
+        for hit in rich_hits:
+            hit.print()
+
+    def test_get_hit_indices_dir(self):
+        path = '../texts/la/lucan.bellum_civile'
+        search_space = make_search_space(path)
+        hits = get_hit_indices('ACROSTIC', 'la', search_space, 'HUQ')
+        self.assertEqual(len(hits), 1)
